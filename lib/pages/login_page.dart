4569d9d4967f423237356652bf5aa6953e52d672
Umair <umair.k2607@gmail.com>
2022-01-06 20:32:44 +0500

    removed unused import

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_catlog/pages/home_page.dart';
 import 'package:flutter_catlog/utils/routes.dart';
 
 class LoginPage extends StatefulWidget {
